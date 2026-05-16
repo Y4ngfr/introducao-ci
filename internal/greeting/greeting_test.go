5799a59baff2091242a7fd6fc74e51611725a8e2
Y4ngfr <yangrodrigues.aluno@unipampa.edu.br>
2026-05-15 23:48:56 -0300

    teste funcional com erro

--- internal/greeting/greeting_test.go
+++ internal/greeting/greeting_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestMessage(t *testing.T) {
 	got := Message("estudantes")
-	want := "Olá, estudantes!"
+	want := "Olá mundo"
 
 	if got != want {
 		t.Fatalf("Message() = %q, want %q", got, want)
